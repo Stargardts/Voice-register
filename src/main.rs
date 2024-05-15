db894145f6d32472629867de5b6a884de27e8398
M. Abdullah <138355502+Abdullah-Jat@users.noreply.github.com>
2024-05-15 12:47:51 +0500

    Trying differnt voices

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use tts::Tts;
+use std::
+    {thread,
+    time::Duration}
+;
 
 
 fn main() {
     let text = "Hello, World!";
 
-    let mut tts = Tts::default().unwrap();
-    Tts::speak(&mut tts, text, false).unwrap();
+    let mut speaker = Tts::default().unwrap();
+
+    let voices = Tts::voices(&speaker).unwrap();
+
+    for voice in voices {
+        // Print English Voices
+        if voice.language() == "en-US" {
+            Tts::set_voice(&mut speaker, &voice).unwrap();
+            Tts::speak(&mut speaker, text, false).unwrap();
+            println!("Voice: {}", voice.name());
+            thread::sleep(Duration::from_secs(1));
+        }
+    }
 }
